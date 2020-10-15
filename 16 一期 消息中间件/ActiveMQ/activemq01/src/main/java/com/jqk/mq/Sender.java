4226a127b5ddb307841e7271a36fde4d07ff650a
smartjiang-java <1677081700@qq.com>
2020-10-16 00:18:31 +0800

    添加activemq代码

---
+++ 16 一期 消息中间件/ActiveMQ/activemq01/src/main/java/com/jqk/mq/Sender.java
@@ -0,0 +1,13 @@
+package com.jqk.mq;
+
+/**
+ * ClassName:Sender
+ * Package:com.jqk.mq
+ * Description:
+ *
+ * @Date:2020/10/16 0:08
+ * @Author:JiangQiKun
+ * @Email:1677081700@qq.com
+ */
+public class Sender {
+}
